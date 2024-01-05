d749675304904d2f3f578c3a0bcd4b38e3fc7baa
Gabriela Gallardo Reyes <148491870+gabrielagallardoreyes@users.noreply.github.com>
2024-01-05 11:21:08 -0300

    Evento onclick

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function cambiar_texto(elemento_this){
+    elemento_this.innerText="Logout";
+}
+function eliminar(elemento){
+    elemento.remove();
+}
+
+function me_gusta(elemento){
+    alert("ninja was liked");
+
+}
+
+// function like(){
+//     var elemento_like= document.querySelector(".bt2")
+//     alert("ninja was liked");
+// }
